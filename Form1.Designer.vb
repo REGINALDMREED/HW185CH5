5d1380d2d85f32fcc2cabe654fc8e0b2ff10bac4
Reed <reginaldmreed@student.midlandstech.edu>
2021-09-28 20:24:14 -0400

    commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWeightConversion
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(138, 407)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(149, 40)
+        Me.btnDisplay.TabIndex = 0
+        Me.btnDisplay.Text = "Display Weight"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(377, 407)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(111, 40)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(21, 43)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(180, 158)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(330, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(277, 22)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Pound and Kilogram Conversion"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(270, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Enter Weight"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(162, 294)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Label3"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(388, 101)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(260, 161)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 111)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Convert Weight"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(58, 84)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(121, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Kilograms to Pounds"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(58, 23)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(121, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Pounds to Kilograms"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'frmWeightConversion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(667, 495)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Name = "frmWeightConversion"
+        Me.Text = "Medical Weight Conversion"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+End Class
---
+++ Form1.vb
@@ -0,0 +1,77 @@
+﻿Private radKTP As Object
+Private radPTK As Object
+Private lblDisplayWeight As Object
+
+Public Class frmWeightConversion
+    Private lblDisplayWeight As Object
+    Private radKTP As Object
+    Private radPTK As Object
+    Private textWeight As Object
+    Private txtWeight As Object
+
+    Private Sub btnDisplay_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplay.Click
+        'to clear textbox, label text and reset radio
+        txtWeight.Clear()
+        lblDisplayWeight.Text = ""
+        radPTK.Checked = True
+        radKTP.Checked = False
+        'Calculation for conversion weight and display its
+        'Declaration section
+        Dim decWeight As Decimal
+        Dim decConvertWeight As Decimal
+        'Did user enter a numeric value?
+        If IsNumeric(txtWeight) Then
+            decWeight = Convert.ToDecimal(txtWeight.Text)
+            'To enter Weight is greater than zero
+            If decWeight > 0 Then
+                'Determine converted weight
+                If radPTK.Checked Then
+                    decConvertWeight = decWeight / 2.2046
+                    'display converted weight in kilograms
+                    lblDisplayWeight.Text = "The Weight of the patient is" & decConvertWeight.ToString("N3") & "
+             kilograms."
+                    ElseIf radPTK.Checked Then
+                    decConvertWeight = decWeight * 2.2046
+                    'display converted weight in pounds
+                    lblDisplayWeight.Text = "The Weight of the patient is" & decConvertWeight.ToString("N3") & "
+                 pounds."
+                End If
+            Else
+                'Display error message if user entered a negative value
+                MsgBox("You Entered" & decWeight.ToString() & ". Enter a Positive Number",, "Input Error")
+                txtWeight.Text = ""
+                txtWeight.Focus()
+            End If
+        Else
+            'Display error message if user enterd invalid numbers
+            MsgBox("You Entered" & decWeight.ToString() & ".Enter a Positive Number",, "Input Error")
+            txtWeight.Text = ""
+            textWeight.Focus()
+        End If
+        Else
+        'Display error message if user entered invalid numbers
+        MsgBox("Enter the Valid Number(s)",, "Input Error")
+        txtWeight.Text = ""
+        txtWeight.Focus()
+        End If
+    End Sub
+
+    Private Function IsNimeric(txtWeight As Object) As Boolean
+        Throw New NotImplementedException()
+    End Function
+End Class
+
+
+Private Sub btnClear_Click(ByVal sender As System, Object, ByVal e As EventArgs) Handles btnClear.Click
+    Dim txtWeight As Object = Nothing
+    'to clear textbox, label text, and reset radio
+    txtWeight.Clear()
+
+    lblDisplayWeight.Text = ""
+
+    radPTK.Checked = True
+
+    radKTP.Checked = False
+End Sub
+
+End Class
